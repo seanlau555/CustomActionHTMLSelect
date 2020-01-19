33099c9600976df3cea26c0047e75abf3cf22f8b
seanlau555 <lthon0225@gmail.com>
2020-01-19 22:20:06 +0800

    feat: javascript update module

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './App';
-import App from './screens'
+// import App from './screens'
+import App from './src'
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
